f374cb9d57dc1e121b094cf0c5087eec6bb276a3
ashrafultusar <ashrafulislamtusar@gmail.com>
2024-11-16 18:47:42 +0600

    added update

--- src/Customer/Componnents/Product/Product.jsx
+++ src/Customer/Componnents/Product/Product.jsx
@@ -21,57 +21,18 @@ import {
 } from "@heroicons/react/20/solid";
 import { mens_kurta } from "../../../Data/MensKurta";
 import ProductCard from "./ProductCard";
+import { filters, singleFilter } from "./FilterData";
+import {
+  FormControl,
+  FormControlLabel,
+  FormLabel,
+  Radio,
+  RadioGroup,
+} from "@mui/material";
 
 const sortOptions = [
-  { name: "Most Popular", href: "#", current: true },
-  { name: "Best Rating", href: "#", current: false },
-  { name: "Newest", href: "#", current: false },
   { name: "Price: Low to High", href: "#", current: false },
   { name: "Price: High to Low", href: "#", current: false },
-];
-const subCategories = [
-  { name: "Totes", href: "#" },
-  { name: "Backpacks", href: "#" },
-  { name: "Travel Bags", href: "#" },
-  { name: "Hip Bags", href: "#" },
-  { name: "Laptop Sleeves", href: "#" },
-];
-const filters = [
-  {
-    id: "color",
-    name: "Color",
-    options: [
-      { value: "white", label: "White", checked: false },
-      { value: "beige", label: "Beige", checked: false },
-      { value: "blue", label: "Blue", checked: true },
-      { value: "brown", label: "Brown", checked: false },
-      { value: "green", label: "Green", checked: false },
-      { value: "purple", label: "Purple", checked: false },
-    ],
-  },
-  {
-    id: "category",
-    name: "Category",
-    options: [
-      { value: "new-arrivals", label: "New Arrivals", checked: false },
-      { value: "sale", label: "Sale", checked: false },
-      { value: "travel", label: "Travel", checked: true },
-      { value: "organization", label: "Organization", checked: false },
-      { value: "accessories", label: "Accessories", checked: false },
-    ],
-  },
-  {
-    id: "size",
-    name: "Size",
-    options: [
-      { value: "2l", label: "2L", checked: false },
-      { value: "6l", label: "6L", checked: false },
-      { value: "12l", label: "12L", checked: false },
-      { value: "18l", label: "18L", checked: false },
-      { value: "20l", label: "20L", checked: false },
-      { value: "40l", label: "40L", checked: true },
-    ],
-  },
 ];
 
 function classNames(...classes) {
@@ -81,6 +42,11 @@ function classNames(...classes) {
 export default function Product() {
   const [mobileFiltersOpen, setMobileFiltersOpen] = useState(false);
 
+    const handelFilter = () => {
+        const searchParams=new URLSearchParams(location.search)
+    }
+    
+    
   return (
     <div className="bg-white">
       <div>
@@ -114,17 +80,6 @@ export default function Product() {
 
               {/* Filters */}
               <form className="mt-4 border-t border-gray-200">
-                <h3 className="sr-only">Categories</h3>
-                <ul role="list" className="px-2 py-3 font-medium text-gray-900">
-                  {subCategories.map((category) => (
-                    <li key={category.name}>
-                      <a href={category.href} className="block px-2 py-3">
-                        {category.name}
-                      </a>
-                    </li>
-                  ))}
-                </ul>
-
                 {filters.map((section) => (
                   <Disclosure
                     key={section.id}
@@ -177,9 +132,9 @@ export default function Product() {
           </div>
         </Dialog>
 
-        <main className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+        <main className="mx-auto px-4 sm:px-6 lg:px-20">
           <div className="flex items-baseline justify-between border-b border-gray-200 pb-6 pt-24">
-            <h1 className="text-4xl font-bold tracking-tight text-gray-900">
+            <h1 className="md:text-4xl font-bold tracking-tight text-gray-900">
               New Arrivals
             </h1>
 
@@ -242,21 +197,12 @@ export default function Product() {
               Products
             </h2>
 
-            <div className="grid grid-cols-1 gap-x-8 gap-y-10 lg:grid-cols-4">
-              {/* Filters */}
+            <div className="grid grid-cols-1 gap-x-8 gap-y-10 lg:grid-cols-5">
+                          {/* Filters */}
+                          <div>
+                          <h1 className="text-lg opacity-50 font-bold">Filters</h1>
+                         
               <form className="hidden lg:block">
-                <h3 className="sr-only">Categories</h3>
-                <ul
-                  role="list"
-                  className="space-y-4 border-b border-gray-200 pb-6 text-sm font-medium text-gray-900"
-                >
-                  {subCategories.map((category) => (
-                    <li key={category.name}>
-                      <a href={category.href}>{category.name}</a>
-                    </li>
-                  ))}
-                </ul>
-
                 {filters.map((section) => (
                   <Disclosure
                     key={section.id}
@@ -265,9 +211,13 @@ export default function Product() {
                   >
                     <h3 className="-my-3 flow-root">
                       <DisclosureButton className="group flex w-full items-center justify-between bg-white py-3 text-sm text-gray-400 hover:text-gray-500">
-                        <span className="font-medium text-gray-900">
+                        <FormLabel
+                          className="text-gray-900"
+                          id="demo-radio-buttons-group-label"
+                        >
                           {section.name}
-                        </span>
+                        </FormLabel>
+
                         <span className="ml-6 flex items-center">
                           <PlusIcon
                             aria-hidden="true"
@@ -282,35 +232,36 @@ export default function Product() {
                     </h3>
                     <DisclosurePanel className="pt-6">
                       <div className="space-y-4">
-                        {section.options.map((option, optionIdx) => (
-                          <div key={option.value} className="flex items-center">
-                            <input
-                              defaultValue={option.value}
-                              defaultChecked={option.checked}
-                              id={`filter-${section.id}-${optionIdx}`}
-                              name={`${section.id}[]`}
-                              type="checkbox"
-                              className="size-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
-                            />
-                            <label
-                              htmlFor={`filter-${section.id}-${optionIdx}`}
-                              className="ml-3 text-sm text-gray-600"
-                            >
-                              {option.label}
-                            </label>
-                          </div>
-                        ))}
+                        <FormControl>
+                          <RadioGroup
+                            aria-labelledby="demo-radio-buttons-group-label"
+                            defaultValue="female"
+                            name="radio-buttons-group"
+                          >
+                            {section.options.map((option, optionIdx) => (
+                              <>
+                                <FormControlLabel
+                                  value={option.id}
+                                  control={<Radio />}
+                                  label={option.label}
+                                />
+                              </>
+                            ))}
+                          </RadioGroup>
+                        </FormControl>
                       </div>
                     </DisclosurePanel>
                   </Disclosure>
                 ))}
               </form>
-
+              </div>
               {/* Product grid */}
-                          <div className="lg:col-span-3 w-full">
-                              <div className="flex flex-wrap justify-center bg-white py-5">
-                                  { mens_kurta.map((item)=><ProductCard></ProductCard>)}
-                              </div>
+              <div className="lg:col-span-4 w-full">
+                <div className="flex flex-wrap justify-center bg-white py-5">
+                  {mens_kurta.map((item) => (
+                    <ProductCard product={item}></ProductCard>
+                  ))}
+                </div>
               </div>
             </div>
           </section>
--- src/Customer/Componnents/Product/ProductCard.jsx
+++ src/Customer/Componnents/Product/ProductCard.jsx
@@ -1,25 +1,27 @@
-import './ProductCard.css'
+import "./ProductCard.css";
 
-const ProductCard = () => {
+const ProductCard = ({ product }) => {
   return (
-    <div className="productCard w-[15rem] m-3 transition-all cursor-pointer">
+    <div className="productCard w-[15rem] m-3 transition-all cursor-pointer border">
       <div className="h-[20rem]">
         <img
           className="h-full w-full object-cover object-left-top"
-          src="https://i.ibb.co.com/fnX19Tx/Front-67eb9273-cc4f-4791-a01e-400a2d8b7817.webp"
+          src={product.imageUrl}
           alt=""
         />
       </div>
 
       <div className="textPart bg-white p-3">
         <div>
-          <p className="font-bold opacity-60">Zara</p>
-          <p>casual puff sleeves solid women shirt</p>
+          <p className="font-bold opacity-60">{product.brand}</p>
+          <p>{product.title}</p>
         </div>
         <div className="flex items-center space-x-2">
-          <p className="font-semibold">$199</p>
-          <p className="line-through opacity-50">$220</p>
-          <p className="text-green-600 font-semibold">70% off</p>
+          <p className="font-semibold">${product.discountedPrice}</p>
+          <p className="line-through opacity-50">${product.price}</p>
+          <p className="text-green-600 font-semibold">
+            ${product.discountPersent}
+          </p>
         </div>
       </div>
     </div>
